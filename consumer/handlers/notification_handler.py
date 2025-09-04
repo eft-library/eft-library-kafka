a2f35330341100958d3faed7dda00fed5133ebd0
poeynus <poeynus@gmail.com>
2025-09-05 07:33:16 +0900

    게시글 알림 일단 개발 완료

--- consumer/handlers/notification_handler.py
+++ consumer/handlers/notification_handler.py
@@ -1,9 +1,23 @@
 import json
+import redis
 from consumer.pg_client import get_pg_connection
 from consumer.logger import logger
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+# PostgreSQL 연결
 pg_conn = get_pg_connection()
 logger.info("알림 DB 연결 성공")
+
+# Redis 연결
+redis_client = redis.StrictRedis(
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
+    db=int(os.getenv("REDIS_DB")),
+    decode_responses=True,
+)
+logger.info("Redis 연결 성공")
 
 
 def process_notification_message(data):
@@ -23,6 +37,7 @@ def process_notification_message(data):
                 SELECT uf.following_email, %(noti_type)s, %(payload)s
                 FROM user_follows uf
                 WHERE uf.follower_email = %(author_email)s
+                RETURNING user_email
             """
             cur.execute(
                 insert_query,
@@ -33,8 +48,20 @@ def process_notification_message(data):
                 },
             )
 
+            # DB에 들어간 user_email 목록 가져오기
+            notified_users = [row[0] for row in cur.fetchall()]
+
         pg_conn.commit()
-        logger.info(f"알림 처리 완료 (DB 한방): {data}")
+
+        # Redis에 알림 데이터 저장
+
+        for user_email in notified_users:
+            redis_key = f"notifications:{user_email}"
+            redis_client.lpush(redis_key, json.dumps(data))
+            # 필요하다면 TTL도 설정 가능 (예: 7일)
+            redis_client.expire(redis_key, 60 * 60 * 24 * 7)
+
+        logger.info(f"알림 처리 완료: {data} (Redis에도 저장됨)")
 
     except Exception as e:
         logger.error(f"알림 처리 실패: {e}")
