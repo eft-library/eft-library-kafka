3f0e7238548748368e3302cbfaa32fda43dcc4fb
poeynus <poeynus@gmail.com>
2025-09-05 16:08:06 +0900

    부모 댓글 알람 추가

--- consumer/handlers/notification_handler.py
+++ consumer/handlers/notification_handler.py
@@ -6,7 +6,6 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# PostgreSQL 연결
 pg_conn = get_pg_connection()
 logger.info("알림 DB 연결 성공")
 
@@ -20,48 +19,61 @@ redis_client = redis.StrictRedis(
 logger.info("Redis 연결 성공")
 
 
+def save_notifications_and_push_to_redis(cur, query, params, data):
+    """
+    공통 함수: DB에 알림 저장 후 Redis에 push
+    """
+    cur.execute(query, params)
+    notified_users = [row[0] for row in cur.fetchall()]
+    pg_conn.commit()
+
+    for user_email in notified_users:
+        redis_key = f"notifications:{user_email}"
+        redis_client.lpush(redis_key, json.dumps(data))
+        redis_client.expire(redis_key, 60 * 60 * 24 * 7)  # TTL 7일
+
+    return notified_users
+
+
 def process_notification_message(data):
     """
-    data 예시:
-    {
-        "url": "123-hello-world",
-        "title": "새 글입니다",
-        "author_email": "writer@test.com",
-        "noti_type": "create_post",
-    }
+    알림 처리하기
     """
     try:
         with pg_conn.cursor() as cur:
-            insert_query = """
-                INSERT INTO user_notifications (user_email, noti_type, payload)
-                SELECT uf.following_email, %(noti_type)s, %(payload)s
-                FROM user_follows uf
-                WHERE uf.follower_email = %(author_email)s
-                RETURNING user_email
-            """
-            cur.execute(
-                insert_query,
-                {
+            if data["noti_type"] == "create_post":
+                # 팔로우한 사용자에게 알림
+                insert_query = """
+                    INSERT INTO user_notifications (user_email, noti_type, payload)
+                    SELECT uf.following_email, %(noti_type)s, %(payload)s
+                    FROM user_follows uf
+                    WHERE uf.follower_email = %(author_email)s
+                    RETURNING user_email
+                """
+                params = {
                     "noti_type": data["noti_type"],
                     "payload": json.dumps(data),
                     "author_email": data["author_email"],
-                },
-            )
+                }
+                save_notifications_and_push_to_redis(cur, insert_query, params, data)
 
-            # DB에 들어간 user_email 목록 가져오기
-            notified_users = [row[0] for row in cur.fetchall()]
+            elif data["noti_type"] == "create_parent_comment":
+                # 게시글 작성자에게 알림
+                insert_query = """
+                    INSERT INTO user_notifications (user_email, noti_type, payload)
+                    SELECT cp.user_email, %(noti_type)s, %(payload)s
+                    FROM community_posts cp
+                    WHERE cp.id = %(post_id)s
+                    RETURNING user_email
+                """
+                params = {
+                    "noti_type": data["noti_type"],
+                    "payload": json.dumps(data),
+                    "post_id": data["post_id"],
+                }
+                save_notifications_and_push_to_redis(cur, insert_query, params, data)
 
-        pg_conn.commit()
-
-        # Redis에 알림 데이터 저장
-
-        for user_email in notified_users:
-            redis_key = f"notifications:{user_email}"
-            redis_client.lpush(redis_key, json.dumps(data))
-            # 필요하다면 TTL도 설정 가능 (예: 7일)
-            redis_client.expire(redis_key, 60 * 60 * 24 * 7)
-
-        logger.info(f"알림 처리 완료: {data} (Redis에도 저장됨)")
+        logger.info(f"알림 처리 완료: {data} (Redis Save)")
 
     except Exception as e:
         logger.error(f"알림 처리 실패: {e}")
